3a95ee3cc7a830b34fabf6ca372456f1dea14ca3
Novish Khalil <novishkhalil2@hotmail.com>
2024-11-11 00:22:10 +0500

    Add files via upload

---
+++ Q14.ts
@@ -0,0 +1,5 @@
+//14.Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d like to invite to dinner. Then use your list to print a message to each person, inviting them to dinner.
+let guest = ["Sana", "Sara", "Riya"];
+console.log("Dear I invited you to dinner", guest[0]);
+console.log("Dear I invited you to dinner", guest[1]);
+console.log("Dear I invited you to dinner", guest[2]);
